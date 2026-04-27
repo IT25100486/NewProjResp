621cf2ed80858936f54eef4e72d5f646548de1a8
Maverick <ayalhewage23@gmail.com>
2026-04-27 23:41:26 +0530

    Main Branch

---
+++ src/main/java/com/we25/mainprj25/MainPrj25Application.java
@@ -0,0 +1,13 @@
+package com.we25.mainprj25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainPrj25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainPrj25Application.class, args);
+    }
+
+}
